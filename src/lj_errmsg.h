93035f72ce415b31f6afd543ca0b45ce08e79a8f
Joshua Ashton <joshua@froggi.es>
2020-10-06 19:48:48 +0100

    Add C-style `/*` and `//` comments

--- src/lj_errmsg.h
+++ src/lj_errmsg.h
@@ -179,6 +179,7 @@ ERRDEF(FFI_CBACKOV,	"too many callbacks")
 ERRDEF(FFI_NYIPACKBIT,	"NYI: packed bit fields")
 ERRDEF(FFI_NYICALL,	"NYI: cannot call this C function (yet)")
 #endif
+ERRDEF(NEVER_ENDING_COMMENT,	"c-style comment reaches end of file")
 
 #if LJ_HASBUFFER
 /* String buffer errors. */
